7b7eca1de61a328ce96d94d41d8b5d01d776c884
barmc <bartoszmiecznikowski853@gmail.com>
2025-01-17 18:23:54 +0100

    readme and comments

--- scripts/scrollToElement.js
+++ scripts/scrollToElement.js
@@ -2,3 +2,5 @@ const scrollToElement = (id) =>{
     let element = document.getElementById(id);
     element.scrollIntoView({behavior: "smooth"})
 }
+
+//użytkownik zostanie przekierowany do elementu na stronie o podanym id
