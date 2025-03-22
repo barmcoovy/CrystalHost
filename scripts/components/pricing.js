8c65cdf4038cc02f6acf1aeae0b1a55c4acb3cf9
barmc <bartoszmiecznikowski853@gmail.com>
2025-03-22 14:13:39 +0100

    switch pricing modified

--- scripts/components/pricing.js
+++ scripts/components/pricing.js
@@ -36,45 +36,36 @@ const pricingPlansSet1 = [
 
 const pricingPlansSet2 = [
   {
-    title: "BRONZE",
+    title: "Gold",
     description: "Lorem ipsum dolor sit amet.",
     price: "24.99 PLN",
+    image: "gold.png",
     details: [
       { header: "AMD Ryzen 9 5950X", detail: "2vCore" },
       { header: "Pamięć RAM", detail: "2 GB" },
       { header: "Szybkość dysku NVMe", detail: "3 GB" },
-      { header: "Własny itemshop", detail: "Tak" },
-      { header: "Anty-DDoS", detail: "Tak" },
-      { header: "Pluginy i Mody", detail: "Bez Limitu" },
-      { header: "Łącza Serwera", detail: "15 Gbps" },
     ],
   },
   {
-    title: "SILVER",
+    title: "Amethyst",
     description: "Lorem ipsum dolor sit amet.",
     price: "34.99 PLN",
+    image: "amethyst.png",
     details: [
       { header: "AMD Ryzen 9 5950X", detail: "3vCore" },
       { header: "Pamięć RAM", detail: "4 GB" },
       { header: "Szybkość dysku NVMe", detail: "6 GB" },
-      { header: "Własny itemshop", detail: "Tak" },
-      { header: "Anty-DDoS", detail: "Tak" },
-      { header: "Pluginy i Mody", detail: "Bez Limitu" },
-      { header: "Łącza Serwera", detail: "25 Gbps" },
     ],
   },
   {
-    title: "GOLD",
+    title: "Bedrock",
     description: "Lorem ipsum dolor sit amet.",
     price: "44.99 PLN",
+    image: "Bedrock.png",
     details: [
       { header: "AMD Ryzen 9 5950X", detail: "5vCore" },
       { header: "Pamięć RAM", detail: "8 GB" },
       { header: "Szybkość dysku NVMe", detail: "12 GB" },
-      { header: "Własny itemshop", detail: "Tak" },
-      { header: "Anty-DDoS", detail: "Tak" },
-      { header: "Pluginy i Mody", detail: "Bez Limitu" },
-      { header: "Łącza Serwera", detail: "50 Gbps" },
     ],
   },
 ];
@@ -118,16 +109,21 @@ function renderPricingPlans(plans) {
   });
 }
 
-pricingSelect.addEventListener("change", (event) => {
-  const selectedValue = event.target.value;
-  if (selectedValue === "set1") {
-    renderPricingPlans(pricingPlansSet1);
-  } else if (selectedValue === "set2") {
-    renderPricingPlans(pricingPlansSet2);
-  }
-});
+// pricingSelect.addEventListener("change", (event) => {
+//   const selectedValue = event.target.value;
+//   if (selectedValue === "set1") {
+//     renderPricingPlans(pricingPlansSet1);
+//   } else if (selectedValue === "set2") {
+//     renderPricingPlans(pricingPlansSet2);
+//   }
+// });
 function toggleSwitch(element) {
   element.classList.toggle("active");
+  if (element.classList.contains("active")) {
+    renderPricingPlans(pricingPlansSet2);
+  } else {
+    renderPricingPlans(pricingPlansSet1);
+  }
 }
 
 // Initial render
